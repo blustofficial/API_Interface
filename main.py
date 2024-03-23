e84035165c89cdae94ce3cf2ce4ea32c7610beb8
blust <blust.0.official@gmail.com>
2024-03-23 21:17:54 +0300

    fix and test neural route

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from dotenv import dotenv_values
 from flask import Flask, jsonify, request
 from marshmallow import Schema, fields, ValidationError
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_core.messages import HumanMessage, SystemMessage
 from pinecone import Pinecone
-from dotenv import dotenv_values
+
 import time
 
 app = Flask(__name__)
@@ -12,16 +14,17 @@ neural = []
 
 config = dotenv_values(".env")
 pc = Pinecone(api_key = config["PINECONE_API_KEY"])
-openAi_key = config["OPENAI_API_KEY"]
-index = pc.Index("secon-1", dimension=1536)
+index = pc.Index('secon-1')
+embeddings = OpenAIEmbeddings(api_key=config["OPENAI_API_KEY"])
 
 class RequiredSchema(Schema):
     ChatId = fields.String(required=True)
-    MessageId = fields.String(required=True)
-    MessageText = fields.String(required=True)
+    MsgId = fields.String(required=True)
+    MsgText = fields.String(required=True)
 
 @app.route('/api/ping', methods = ['GET'])
 def Ping():
+    print('pong')
     return jsonify({'message': 'pong'})
 
 """
@@ -46,25 +49,50 @@ def AddToQueue():
 """
 Neural Routes
 """
+# @app.route('/api/neural', methods = ['GET'])
+# def GetNeural():
+#     return jsonify({'neural': neural})
+
 @app.route('/api/neural', methods = ['GET'])
-def GetNeural():
-    return jsonify({'neural': neural})
-
-@app.route('/api/neural', methods = ['POST'])
 def AddToNeural():
-    data = request.get_json()
-    schema = RequiredSchema()
+    # ПОМЕНЯТЬ МЕТОД НА POST!!!
+    # data = request.get_json()
+    # schema = RequiredSchema()
+    # try:
+    #     result = schema.load(data)
+    # except ValidationError as e:
+    #     return "Bad Request", 400
+
     try:
-        result = schema.load(data)
-    except ValidationError as e:
-        return "Bad Request", 400
+        text = "потерял ключи"
+        vector = embeddings.embed_query(text)
 
-    neural.append((result['ChatId'], result['MessageId'], result['MessageText']))
+        # vector = embeddings.embed_query(str(result['MsgText']))
+        response = index.query(vector=vector, top_k=10, include_metadata=True)
 
-    embeddings = OpenAIEmbeddings(api_key=openAi_key)
-    vector = embeddings.embed_query(str(result))
-    index.upsert(vectors=[{"id": f"{time.time()}", "values": vector}], )
-    return jsonify({'total_entities_neural': f'{len(neural)}.'})
+        metadata = [{'id': match['id'], 'metadata': match['metadata']} for match in response['matches']]
+        data = metadata[0]['metadata']['genre']
+
+        chat = ChatOpenAI(
+            temperature=0.6,
+            openai_api_key=config["OPENAI_API_KEY"]
+        )
+
+        promt = "Верни мне максимально подходящие данные по объявлению, мне нужен формат json в возможном виде нескольких объектов, в json должен состоять из chat_id, msg_id и content"
+
+        messages = [
+            SystemMessage(
+                content=promt
+            ),
+            HumanMessage(
+                content=data
+            ),
+        ]
+
+        content = chat.invoke(messages).content
+        return jsonify({'content': content})
+    except Exception as e:
+        print("Analny tanets:\n", e)
 
 if __name__ == '__main__':
     print("was started")
