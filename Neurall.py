fb9de9e233c09241ad91ed6a2d0122729e06c5bb
blust <blust.0.official@gmail.com>
2024-03-24 17:23:47 +0300

    fix route

--- Neurall.py
+++ Neurall.py
@@ -38,25 +38,3 @@ class NerualClass:
         ]
         content = self.chat.invoke(messages).content
         return content
-
-
-if __name__ == "__main__":
-    pinecone_api_key = "159e533b-5d20-4f3c-93c1-5277f3df9cdb"
-    openai_api_key = "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"
-
-    neural_class = NerualClass(pinecone_api_key, openai_api_key)
-
-    # чтобы добавить данные
-    text = '''{"chat_id": -1001242071007, "msg_id": 18109, "content":"Были утерены ключи район колычво примерно детсад:салнышко или ул астахова просьба позванить 89017573245"}
-{"chat_id": -1001242071007, "msg_id": 18106, "content":"Найдена по адресу Коломна пр-кт Кирова 56"}
-{"chat_id": -1001242071007, "msg_id": 18105, "content":"Друзья кто хочет заработать норм денег от 5.000р не чего сложного нет пишите мне в лс"}
-{"chat_id": -1001242071007, "msg_id": 18104, "content":"Друзья кто хочет заработать норм денег от 5.000р не чего сложного нет пишите мне в лс"}
-{"chat_id": -1001242071007, "msg_id": 18100, "content":"Найдена возле дома 56 по пр-кт Кирова у 1 подъезда"}
-{"chat_id": -1001242071007, "msg_id": 18097, "content":"Нашли серьгу Весенняя 20"}'''
-    neural_class.process_data(text)
-
-    # запрос к данным
-    query_text = "потерял ключи"
-    result = neural_class.ask_question(query_text)
-    result = neural_class.invoke_chat(query_text, result)
-    print(result)
